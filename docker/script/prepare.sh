78d723289eab2081de3285549532589e4e28f38c
pr0s1k <iprosvirnin@bk.ru>
2024-04-20 00:28:41 +0400

    deploy 0.24

--- docker/script/prepare.sh
+++ docker/script/prepare.sh
@@ -1,7 +1,9 @@
+php artisan migrate
+
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
 php artisan optimize
+
 php artisan storage:link
-php artisan migrate
